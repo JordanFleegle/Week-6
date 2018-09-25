407a3b9f79871035a7db584988899fca06f43fab
JordanFleegle <35332498+JordanFleegle@users.noreply.github.com>
2018-09-24 19:06:46 -0500

    app working, odd back button behavior

--- HideKeyBoard/ViewController.swift
+++ HideKeyBoard/ViewController.swift
@@ -22,12 +22,12 @@ class ViewController: UIViewController {
     
     //actions
     @IBAction func btnBack(_ sender: UIButton) {
-        scrollView.setContentOffset(CGpoint.zero, animated: true)
+        ScrollView.setContentOffset(CGPoint.zero, animated: true)
         
         txtFirst.becomeFirstResponder()
     }
     @IBAction func btnSave(_ sender: UIButton) {
-        DismissKeyBoard()
+        DismissKeyboard()
         if (txtFirst.text=="" || txtLast.text=="" || txtEmail.text=="") {
             MsgBox("All fields required, please correct.")
         }
@@ -40,15 +40,75 @@ class ViewController: UIViewController {
             txtFirst.text = ""
             txtLast.text = ""
             txtEmail.text = ""
+            
+            var scrollPoint:CGPoint
+            scrollPoint = CGPoint (x:0, y:btnBack.frame.origin.y)
+            ScrollView.setContentOffset(scrollPoint, animated: true)
         }
     }
     @IBAction func btnView(_ sender: UIButton) {
+        DismissKeyboard()
+        var scrollPoint:CGPoint
+        scrollPoint = CGPoint (x:0, y:btnBack.frame.origin.y)
+        ScrollView.setContentOffset(scrollPoint, animated: true)
     }
+    
+    func MsgBox (_ message:String){
+        //Create Alert
+        let alert = UIAlertView()
+        alert.title = "Alert"
+        alert.message = message
+        alert.addButton(withTitle: "OK")
+        alert.show()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtFirst.endEditing(true)
+        txtLast.endEditing(true)
+        txtEmail.endEditing(true)
+    }
+    
+    @objc func DismissKeyboard() {
+        txtFirst.endEditing(true)
+        txtLast.endEditing(true)
+        txtEmail.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    func textFieldDidBeginEditing(_ textField:UITextField) {
+        var scrollPoint:CGPoint
+        scrollPoint = CGPoint(x:0, y:textField.frame.origin.y)
+        ScrollView.setContentOffset(scrollPoint, animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField:UITextField) {
+        ScrollView.setContentOffset(CGPoint.zero, animated: true)
+    }
+    
+    func textViewDidBeginEditing(_ textField:UITextView) {
+        var scrollPoint:CGPoint
+        scrollPoint = CGPoint(x:0, y:textField.frame.origin.y)
+        ScrollView.setContentOffset(scrollPoint, animated: true)
+    }
+    
+    func textViewDidEndEditing(_ textField:UITextView) {
+        ScrollView.setContentOffset(CGPoint.zero, animated: true)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(ViewController.DismissKeyboard))
+        ScrollView.addGestureRecognizer(tap)
+        let size: CGRect = UIScreen.main.bounds
+        ScrollView.frame = CGRect(x:0, y:50, width:size.width, height:size.height)
+        txtFirst.becomeFirstResponder()
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
